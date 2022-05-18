0932e3151275327a23fc601c6d770aed290358fa
Sina <sina.alsharif@gmail.com>
2022-05-18 15:46:20 +1000

    Update comments

--- main/Pricebucket.hpp
+++ main/Pricebucket.hpp
@@ -4,11 +4,8 @@
 #include <deque>
 #include "Order.hpp"
 
-struct depth {
-	float price;
-	int numBids;
-	int numAsks;
-};
+/* Data class to standardise depth format */
+struct depth { float price; int numBids; int numAsks; };
 
 class Pricebucket {
 	private:
@@ -18,11 +15,22 @@ class Pricebucket {
 
 	public:
 		Pricebucket(float p) : price{p} {}
+
+		/* Add an order to the price bucket */
 		void addOrder(Order*);
+
+		/* Remove an order from the price bucket */
 		int removeOrder(Order*);
-		void findBid(Order*);
-		void findAsk(Order*);
+
+		/* Find an order in the bid or ask list
+		 The side (bid or ask) of the order is specified within the class
+		 */
+		Order* findOrder(Order*);
+
+		/* Get the number of bids for this price bucket*/
 		int sizeBid();
+
+		/* Get the number of asks for this price bucket */
 		int sizeAsk();
 };
 
@@ -31,8 +39,13 @@ class PricebucketManager {
 		std::map<float, Pricebucket> buckets;
 	
 	public:
+		/* Add a price to the price buckets */
 		Pricebucket * addPrice(float price);
-		Pricebucket * findPrice(float price);
+
+		/* Find a price in the price buckets, returned as copy to prevent tampering */
+		Pricebucket findPrice(float price);
+
+		/* Return all buckets as a copy */
 		std::map<float, Pricebucket> getBuckets();
 };
 
