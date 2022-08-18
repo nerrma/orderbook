8fbee655e1ed2c8997f283c733204f043c178411
Sina <sina.alsharif@gmail.com>
2022-08-18 18:27:30 +1000

    add order id

--- source/client.cpp
+++ source/client.cpp
@@ -2,11 +2,12 @@
 #include <order/order.hpp>
 
 auto main() -> int {
-	auto o = order::order(order::side::ask, 140, 1180);
+	auto o = order::order(1, order::side::ask, 140, 1180);
 	auto i = order::order(std::move(o));
 
 	auto d = o.get_data();
 
+	std::cout << d.id << std::endl;
 	std::cout << d.amt << std::endl;
 	std::cout << d.price << std::endl;
 	std::cout << d.order_side << std::endl;
--- test/order/test_basic.cpp
+++ test/order/test_basic.cpp
@@ -3,9 +3,9 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("basic operator test") {
-	auto o1 = order::order(order::side::ask, 140, 10);
-	auto o2 = order::order(order::side::ask, 180, 10);
-	auto o3 = order::order(order::side::ask, 180, 20);
+	auto o1 = order::order(1, order::side::ask, 140, 10);
+	auto o2 = order::order(2, order::side::ask, 180, 10);
+	auto o3 = order::order(3, order::side::ask, 180, 20);
 
 	CHECK(o1 == o1);
 
@@ -14,7 +14,7 @@ TEST_CASE("basic operator test") {
 }
 
 TEST_CASE("struct test") {
-	auto o1 = order::order(order::side::ask, 140, 10);
+	auto o1 = order::order(1, order::side::ask, 140, 10);
 
-	CHECK(o1.get_data() == order::order_data{order::side::ask, 140, 10});
+	CHECK(o1.get_data() == order::order_data{1, order::side::ask, 140, 10});
 }
