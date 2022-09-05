6bededf31acebf2b12a29a3b2b11d04c61302b81
Sina <sina.alsharif@gmail.com>
2022-09-05 21:08:21 +1000

    blank out client

--- source/client.cpp
+++ source/client.cpp
@@ -2,12 +2,4 @@
 #include <iostream>
 #include <order/order.hpp>
 
-auto main() -> int {
-	auto o = order::order(1, order::side::ask, 140, 1180);
-
-	auto ob = orderbook();
-	ob.add_order(o);
-	std::cout << ob.get_volume(order::side::ask, 1180) << std::endl;
-
-	return 0;
-}
+auto main() -> int {}
